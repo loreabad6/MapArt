78f1955123fbe25028528a67a3ad4791355d50a7
loreabad6 <lore.abad6@gmail.com>
2019-09-25 19:11:25 +0200

    first commit

---
+++ data.R
@@ -0,0 +1,41 @@
+library(sf)
+library(ggmap)
+library(osmdata)
+library(osmplotr)
+library(dplyr)
+bbox_select <- getbb('muenster, de') 
+bbox_select[,] <- c(7.56, 51.90, 7.7, 52.00)
+# muenster: 7.56, 51.90, 7.7, 52.00
+# salzburg: 12.9, 47.73, 13.12, 47.87
+# lisbon: -9.21, 38.65, -9.03, 38.81
+
+# dat_B <- extract_osm_objects (key = 'building', bbox = bbox_select)
+dat_H <- extract_osm_objects (key = 'highway', bbox = bbox_select)
+dat_R <- extract_osm_objects (key = 'water', bbox = bbox_select)
+# dat_C <- extract_osm_objects(key = 'natural', value = 'coast', bbox = bbox_select, return_type = 'line')
+# dat_W <- osm_line2poly(dat_C, bbox = bbox_select)
+# "bridleway","construction","corridor","cycleway","footway",      
+# "living_street","motorway","motorway_link","path","pedestrian",    
+# "platform","primary","primary_link","private_footway","proposed",
+# "residential","road","secondary","secondary_link","service",       
+# "steps","tertiary","tertiary_link","track","trunk",     
+# "trunk_link","unclassified"
+
+dat_H_1 <- dat_H %>% 
+  filter(highway %in% c("motorway","motorway_link","primary","primary_link",
+                        "secondary","secondary_link","tertiary","tertiary_link", 
+                        "trunk","trunk_link"))
+dat_H_2 <- dat_H %>% 
+  filter(highway %in% c("bridleway","cycleway","footway",      
+                        "living_street","path","pedestrian",
+                        "residential","road","track","unclassified"))
+map <- osm_basemap (bbox = bbox_select, bg = 'white')
+
+map <- add_osm_objects (map, dat_H_1, col = 'gray50', border = NA, size = 0.5)
+map <- add_osm_objects (map, dat_H_2, col = 'gray50',  border = NA, size = 0.2)
+# map <- add_osm_objects (map, dat_C, col = 'gray50', border = NA, size = 0.2)
+map <- add_osm_objects (map, dat_R, col = 'gray20', border = NA)
+
+print_osm_map (map)
+print_osm_map (map, dpi = 900, filename = 'muenster.png')
+# mapcheck <- get_stamenmap(bbox = bbox_select, zoom = 11, maptype = 'toner-lite', source = 'stamen', crop = T)
